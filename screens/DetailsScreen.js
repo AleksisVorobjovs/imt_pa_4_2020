a51f5be62ff939b1d7ed5e25bb264dad904e8aca
AleksisVorobjovs <73535402+AleksisVorobjovs@users.noreply.github.com>
2020-12-09 02:14:14 +0200

    Updating text error
    Now can be seen on mobile as well

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -9,8 +9,8 @@ export default function DetailsScreen({route}) {
   return (
     <View>
       <Text style={{fontSize: 20, marginBottom: 20}}>{JSON.stringify(title)}</Text>
-      <Text style={{ flex: 1, fontSize: 15, alignItems: 'center', marginBottom: 20, justifyContent: 'center' }}>{JSON.stringify(description)}</Text>
-      <Text style={{ flex: 1, fontSize: 15, alignItems: 'center', justifyContent: 'center', color:'#00F' }}>URL: {JSON.stringify(url)}</Text>
+      <Text style={{ fontSize: 10, marginBottom: 20 }}>{JSON.stringify(description)}</Text>
+      <Text style={{ fontSize: 10 }}>URL: {JSON.stringify(url)}</Text>
     </View>
   );
 }
